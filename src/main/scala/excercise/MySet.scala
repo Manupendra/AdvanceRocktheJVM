737d0faac1f22c5243f8d66dacbefc3bb01f027d
manupendratiwari <manupendratiwari@gmail.com>
2023-09-23 16:08:54 +0530

    Added Exercise

--- src/main/scala/excercise/MySet.scala
+++ src/main/scala/excercise/MySet.scala
@@ -1,5 +1,19 @@
 package excercise
 
-trait MySet {
+trait MySet[A] extends (A=> Boolean) {
 
+  /*
+  * EXERCISE - implement a functional set
+  * */
+  def apply(elem: A): Boolean =
+    contains(elem)
+
+  def contains(elem: A): Boolean
+  def +(elem: A): MySet[A]
+  def ++(anotherSet: MySet[A]): MySet[A] // union
+
+  def map[B](f: A => B): MySet[B]
+  def flatMap[B](f: A => MySet[B]): MySet[B]
+  def filter(predicate: A => Boolean): MySet[A]
+  def foreach(f: A => Unit): Unit
 }
